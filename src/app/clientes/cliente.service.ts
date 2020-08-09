edd72e7532e5368a76b03a32a1e0cf914cadbdb2
Javier Tobar <javiertobarvera@gmail.com>
2020-08-09 03:43:38 -0500

    Primer commit

---
+++ src/app/clientes/cliente.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Cliente } from './cliente';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ClienteService {
+  private urlEndpoint: string = 'http://localhost:8080/api/clientes';
+  constructor(private http: HttpClient) { }
+
+  getClientes(): Observable<Cliente[]> {
+    return this.http.get(this.urlEndpoint).pipe(
+      map((response) => response as Cliente[])
+    )
+  }
+}
